d595498c6ca773e0dc95adeea2f7bdbc6b334730
Sam <sam.dawson181@gmail.com>
2019-06-25 20:15:53 +1000

    Fix interpolation

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -7,7 +7,7 @@ const spacing = {
 
 const systemFont = `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`
 const fonts = {
-    header: 'Muli, ${systemFont}',
+    header: `Muli, ${systemFont}`,
     content: systemFont
 }
 
