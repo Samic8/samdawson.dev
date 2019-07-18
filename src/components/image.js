ba9d907ee8faade48aeaf35e48b7af8edb20b6ed
Sam Dawson <sam.dawson181@gmail.com>
2019-07-18 19:01:46 +1000

    Make more rounded

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ const Image = () => {
     }
   `)
 
-  return <Img className={'max-w-xs rounded'} fluid={data.placeholderImage.childImageSharp.fluid} />
+  return <Img className={'max-w-xs rounded-lg'} fluid={data.placeholderImage.childImageSharp.fluid} />
 }
 
 export default Image
