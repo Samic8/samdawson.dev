e20c71b3fb3476f33d0284d8976d05f73f2b341e
Sam Dawson <sam.dawson181@gmail.com>
2019-07-25 19:37:39 +1000

    Reduce shadow

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,7 +31,7 @@ const Header = () => {
       }
     >
       <Row className={"flex sm:block items-start flex-grow"}>
-        <Link to={"/"} className={'shadow-lg inline-block'}>
+        <Link to={"/"} className={'sm:shadow-md inline-block'}>
           <HeadshotImage />
         </Link>
         <div
