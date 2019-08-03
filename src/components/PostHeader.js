6d2948d215a91336d6ed95dc1fcfafdf3b5dfd1f
Sam Dawson <sam.dawson181@gmail.com>
2019-08-03 18:25:23 +1000

    Change header style

--- src/components/PostHeader.js
+++ src/components/PostHeader.js
@@ -4,9 +4,9 @@ import { techToBgColor } from "../utility/techColors";
 export default function PostHeader({ title, tech }) {
   return (
     <div className={"flex justify-between"}>
-      <h1 className="font-bold text-gray-500 text-lg leading-tight my-10 sm:my-20 text-center max-w-3xl mx-auto relative">
+      <h1 className="font-bold text-gray-500 left text-lg leading-tight my-10 sm:my-20 text-center max-w-3xl mx-auto relative">
         {title}
-        <div className={`w-1/3 h-2 absolute left-0 mt-2 rounded-sm ${techToBgColor[tech] || 'bg-gray-500'}`} />
+        <div className={`w-24 h-1 absolute mt-2 ${techToBgColor[tech] || 'bg-gray-200'}`} style={{ left: '0' }} />
       </h1>
     </div>
   )
