01802f4091aacb7af61b4d244edaad4d3a1d0552
Sam Dawson <sam.dawson181@gmail.com>
2019-08-05 17:57:59 +1000

    Whitelist article styles

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,20 @@
 const tailwind = require('tailwindcss')
 
 const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: [
-      './src/**/*.js',
-      './node_modules/prismjs/prism.js'
-    ],
-    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-    whitelistPatternsChildren: [/post.*/, /body/, /html/]
-  })
-  
+  content: [
+    './src/**/*.js',
+    './node_modules/prismjs/prism.js'
+  ],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  whitelistPatternsChildren: [/article.*/, /body/, /html/]
+})
+
 module.exports = () => ({
-    plugins: [
-        tailwind('./tailwind.config.js'),
-        require('postcss-nested'),
-        require('postcss-extend-rule')(),
-        require('autoprefixer'),
-        ...process.env.NODE_ENV === 'production' ? [purgecss] : []
-    ],
+  plugins: [
+    tailwind('./tailwind.config.js'),
+    require('postcss-nested'),
+    require('postcss-extend-rule')(),
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production' ? [purgecss] : []
+  ],
 })
