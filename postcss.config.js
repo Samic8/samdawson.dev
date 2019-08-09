d07389f707d3910629273e49ea15aa60b158825d
Sam Dawson <sam.dawson181@gmail.com>
2019-08-09 21:15:58 +1000

    Check generated html in purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,21 @@
-const tailwind = require('tailwindcss')
+const tailwind = require("tailwindcss")
 
-const purgecss = require('@fullhuman/postcss-purgecss')({
+const purgecss = require("@fullhuman/postcss-purgecss")({
   content: [
-    './src/**/*.js',
-    './node_modules/prismjs/prism.js'
+    "./src/**/*.js",
+    "./node_modules/prismjs/prism.js",
+    "./public/**/*.html",
   ],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-  whitelistPatternsChildren: [/article.*/, /body/, /html/]
+  whitelistPatternsChildren: [/article.*/, /body/, /html/],
 })
 
 module.exports = () => ({
   plugins: [
-    tailwind('./tailwind.config.js'),
-    require('postcss-nested'),
-    require('postcss-extend-rule')(),
-    require('autoprefixer'),
-    ...process.env.NODE_ENV === 'production' ? [purgecss] : []
+    tailwind("./tailwind.config.js"),
+    require("postcss-nested"),
+    require("postcss-extend-rule")(),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
   ],
 })
