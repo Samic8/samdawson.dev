6f5b18a5cbb65498f6421c4ef3264e25de36133d
Sam Dawson <sam.dawson181@gmail.com>
2020-02-15 11:35:04 +1100

    Center header text

--- src/components/ArticleHeader.js
+++ src/components/ArticleHeader.js
@@ -3,11 +3,13 @@ import Time from "./Time"
 
 export default function ArticleHeader({ title, date, dateTime }) {
   return (
-    <div className={"max-w-xl mx-auto my-10 sm:my-16"}>
+    <div
+      className={"flex flex-col items-center max-w-xl mx-auto my-10 sm:my-16"}
+    >
       <h1 className="font-bold text-gray-900 text-lg leading-tight max-w-xl mx-auto relative font-extrabold">
         {title}
+        <Time className="mt-2 self-start" date={date} dateTime={dateTime} />
       </h1>
-      <Time className="mt-2" date={date} dateTime={dateTime} />
     </div>
   )
 }
