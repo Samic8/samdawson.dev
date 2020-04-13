3003de728ef2a924eef6c468995f3177665b168d
Sam Dawson <sam.dawson181@gmail.com>
2020-04-13 16:30:48 +1000

    Only parse cookies if they exist

--- functions/quick-feedback/quick-feedback.js
+++ functions/quick-feedback/quick-feedback.js
@@ -5,8 +5,9 @@ const id = "UA-93793174-3"
 exports.handler = (event, context, callback) => {
   const type = event.queryStringParameters.type
   const page = event.queryStringParameters.page
+  const cookies = event.headers.cookie
 
-  const { _ga: clientId } = cookie.parse(event.headers.cookie) || {}
+  const { _ga: clientId } = cookies ? cookie.parse(cookies) : {}
   const visitor = clientId ? ua(id, clientId) : ua(id)
 
   // Logging for netlify dev console
