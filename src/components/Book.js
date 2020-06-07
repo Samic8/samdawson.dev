9589b58dfa364e52af9e1f97d757b11323a43bac
Sam Dawson <sam.dawson181@gmail.com>
2020-06-08 09:30:17 +1000

    Change book style

--- src/components/Book.js
+++ src/components/Book.js
@@ -20,8 +20,12 @@ export default function Book({ className }) {
   return (
     <div
       aria-hidden
+      style={{
+        boxShadow:
+          "5px 10px 15px -3px rgba(0, 0, 0, 0.1), 5px 4px 6px -2px rgba(0, 0, 0, 0.05)",
+      }}
       className={getActiveClasses(
-        "hover:scale-105 transform transition-transform ease-in duration-150 relative overflow-hidden text-white flex flex-shrink-0 font-bold p-2 w-48 h-64 border-gray-700 border-4 border-solid rounded-sm",
+        "hover:scale-105 transform transition-transform ease-in duration-150 relative text-white flex flex-shrink-0 font-bold p-2 w-48 h-64 rounded-t-md overflow-hidden rounded-bl-md rounded-br-sm",
         className
       )}
     >
@@ -31,6 +35,14 @@ export default function Book({ className }) {
       </div>
       <div className="top-0 bottom-0 left-0 right-0 z-0 absolute">
         <Img fluid={hexImage.childImageSharp.fluid} />
+      </div>
+      <div className="border-gray-700 border-2 border-solid h-3 absolute bottom-0 left-0 right-0 flex flex-col justify-evenly overflow-hidden bg-white rounded-t-sm rounded-bl-md">
+        <div className="border-gray-200 border-t border-solid w-full"></div>
+        <div className="border-gray-200 border-t border-solid w-full"></div>
+        <div className="border-gray-200 border-t border-dotted w-full"></div>
+        <div className="border-gray-200 border-t border-solid w-full"></div>
+        <div className="border-gray-200 border-t border-dotted w-full"></div>
+        <div className="border-gray-200 border-t border-solid w-full"></div>
       </div>
     </div>
   )
